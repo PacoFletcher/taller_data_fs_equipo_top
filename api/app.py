fd122786d46deac72dd099a4dd108fd8ea46d511
PacoFletcher <pacofletcher@gmail.com>
2025-05-22 10:48:41 +0200

    api return dummy

--- api/app.py
+++ api/app.py
@@ -11,5 +11,23 @@ def home():
     return jsonify({'message': 'Welcome to the housing API!'})
 
 
+# Cargar la base de datos en un DataFrame
+model = pd.read_pickle('models/model.pkl')
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json()
+
+    surface = int(data['surface'])
+    bedrooms = int(data['bedrooms'])
+    restrooms = int(data['restrooms'])
+
+    input_data = [[surface, bedrooms, restrooms]]
+    prediction = model.predict(input_data)
+
+    return 1000
+    ### Versión definitiva
+    # return jsonify({'prediction': float(prediction[0])})
+    ###
+
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
